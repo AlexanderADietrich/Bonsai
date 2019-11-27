5c81f9f9585a5076a47abc17c9409f40239d8561
ALEXANDER DIETRICH <alexander.a.dietrich@gmail.com>
2019-11-26 16:50:17 -0800

    Collision / Lighting

--- Assets/Source/Model/GrowingSceneNode.cs
+++ Assets/Source/Model/GrowingSceneNode.cs
@@ -6,12 +6,15 @@ public class GrowingSceneNode : SceneNode
 {
     public Material nodeMaterial = null;
     public Shader nodeShader = null;
+    public CollisionChamber reportTo = null;
     int growthRate = 150;
     int current = 0;
     int gen = 0;
     static int MAX_GEN = 7;
     GameObject child1;
     GameObject child2;
+    public NodePrimitive geom = null;
+    public int mode = 0;
 
     // Update is called once per frame
     void Update()
@@ -30,6 +33,11 @@ public class GrowingSceneNode : SceneNode
         child1 = createChild(true);
         child2 = createChild(false);
     }
+    
+    public Vector3[] getCollisionSegment()
+    {
+        return geom.getColSegmentCylinder(transform.rotation, transform.localScale);
+    }
 
     public static float decayRate = 0.9f;
     GameObject createChild(bool negate)
@@ -41,6 +49,9 @@ public class GrowingSceneNode : SceneNode
         c1.GetComponent<GrowingSceneNode>().nodeMaterial = nodeMaterial;
         c1.GetComponent<GrowingSceneNode>().nodeShader = nodeShader;
         c1.GetComponent<GrowingSceneNode>().gen = gen + 1;
+        reportTo.plantParts.Add(c1.GetComponent<GrowingSceneNode>());
+        c1.GetComponent<GrowingSceneNode>().reportTo = reportTo;
+        c1.GetComponent<GrowingSceneNode>().mode = mode;
         c1.transform.parent = this.transform;
         //initialize game object
         GameObject c1geom = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
@@ -50,16 +61,67 @@ public class GrowingSceneNode : SceneNode
         GameObject geom = new GameObject();
         geom.name = "Geom";
         geom.transform.parent = c1.transform;
+        c1.GetComponent<GrowingSceneNode>().geom = c1geom.GetComponent<NodePrimitive>();
         c1geom.transform.parent = geom.transform;
         c1.GetComponent<GrowingSceneNode>().PrimitiveList.Add(c1geom.GetComponent<NodePrimitive>());
         //change scale/rotation/material/location/origin position/shader
-        c1.transform.localRotation = Quaternion.Euler(0, 0, (negate) ? -45 : 45);
-        c1.transform.localScale = transform.localScale * decayRate;
-        c1.transform.localPosition = transform.localPosition;
-        c1.GetComponent<GrowingSceneNode>().NodeOrigin = new Vector3(((negate) ? 1 : -1) * Mathf.Sqrt(2) * 0.5f * decayRate, transform.localScale.y + Mathf.Sqrt(2) * 0.5f * decayRate, 0);
+        if (mode == 0)
+        {
+            c1.transform.localRotation = Quaternion.Euler(0, 0, (negate) ? -45 : 45);
+            c1.transform.localScale = transform.localScale * decayRate;
+            c1.transform.localPosition = transform.localPosition;
+            c1.GetComponent<GrowingSceneNode>().NodeOrigin 
+                = new Vector3(
+                    ((negate) ? 1 : -1) * Mathf.Sqrt(2) * 0.5f * decayRate,
+                    transform.localScale.y + Mathf.Sqrt(2) * 0.5f * decayRate, 
+                    0);
+        }
+        else if (mode == 1)
+        {
+            if (gen % 2 == 0)
+            {
+                c1.transform.localRotation = Quaternion.Euler(0, (negate) ? 180 : 0, 45);
+                c1.transform.localScale = transform.localScale * decayRate;
+                c1.transform.localPosition = transform.localPosition;
+                c1.GetComponent<GrowingSceneNode>().NodeOrigin
+                    = new Vector3(
+                        ((negate) ? 1 : -1) * Mathf.Sqrt(2) * 0.5f * decayRate,
+                        transform.localScale.y + Mathf.Sqrt(2) * 0.5f * decayRate,
+                        0);
+            } 
+            else
+            {
+                if (!negate)
+                {
+                    c1.transform.localRotation = Quaternion.Euler(0, 0, 0);
+                    c1.transform.localScale = transform.localScale * decayRate;
+                    c1.transform.localPosition = transform.localPosition;
+                    c1.GetComponent<GrowingSceneNode>().NodeOrigin
+                        = new Vector3(
+                            0,
+                            transform.localScale.y + decayRate,
+                            0);
+                }
+                else
+                {
+                    c1.transform.localRotation = Quaternion.Euler(0, 0, (negate) ? -45 : 45);
+                    c1.transform.localScale = transform.localScale * decayRate;
+                    c1.transform.localPosition = transform.localPosition;
+                    c1.GetComponent<GrowingSceneNode>().NodeOrigin
+                        = new Vector3(
+                            ((negate) ? 1 : -1) * Mathf.Sqrt(2) * 0.5f * decayRate,
+                            transform.localScale.y + Mathf.Sqrt(2) * 0.5f * decayRate,
+                            0);
+                }
+                
+            }
+                
+            
+        }
+
         nodeMaterial.shader = nodeShader;
         c1geom.GetComponent<Renderer>().material = nodeMaterial;
-
+        
         return c1;
     }
 }
